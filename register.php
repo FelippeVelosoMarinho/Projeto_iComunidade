c5f72c98740e68cf7cc2c8542792a2a3f1eaae2c
Felippe Veloso <60450622+FelippeVelosoMarinho@users.noreply.github.com>
2023-08-12 23:08:02 -0300

    feat: iniciando crud

---
+++ register.php
@@ -0,0 +1,33 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+
+require_once('api.php');
+session_start();
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+$turma = $_POST['turma'];
+
+$str = "SELECT * FROM aluno WHERE email = '$email'";
+
+$response = $conn->query($str);
+
+if ($response->num_rows > 0) {
+    echo json_encode(["message"=>"Email já cadastrado"]);
+} else {
+
+        $sql = "INSERT INTO aluno(nome, email, senha, turma) VALUES('".$nome."', '".$email."', '".$senha."', '".$turma."')";
+
+        $result = $conn->query($sql);
+
+        if (!$result) {
+            http_response_code(500);
+            echo json_encode(["error" => "Erro ao cadastrar usuário"]);
+        } else {
+            http_response_code(200);
+            echo json_encode(["success" => "Usuário cadastrado com sucesso"]);
+        }
+    }
+?>
